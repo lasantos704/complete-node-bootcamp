ed24fa42f8980931aed4f6d994512f75c4ec322b
Leonardo A. Pinheiro <lasantos704@gmail.com>
2026-01-12 18:37:34 +0000

    section 8 class 95

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -2,8 +2,24 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
 
+    console.log(req.query, queryObj);
+
+    const query = Tour.find(queryObj);
+    // const tours = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
@@ -22,7 +38,7 @@ exports.getAllTours = async (req, res) => {
 exports.getTour = async (req, res) => {
   try {
     const tour = await Tour.findById(req.params.id);
-    // Tour.findOne({_id: req.params.id}) 
+    // Tour.findOne({_id: req.params.id})
 
     res.status(200).json({
       status: 'success',
@@ -53,7 +69,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: 'fail',
-      message: 'Invalid data sent!',
+      message: error,
     });
   }
 };
