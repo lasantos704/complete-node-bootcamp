f6d0dc720d67696817caed7ac4319585b1def7c2
Leonardo A. Pinheiro <lasantos704@gmail.com>
2026-02-02 18:17:55 +0000

    section 10 class 129

--- natours/controllers/authController.js
+++ natours/controllers/authController.js
@@ -1,13 +1,39 @@
+const jwt = require('jsonwebtoken');
 const catchAsync = require('../utils/catchAsync');
 const User = require('./../models/userModel');
+const AppError = require('../utils/appError');
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  // Create on config.env 32 chars - JWT_SECRET= and JWT_EXPIRES_IN=30d
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: process.in.JWT_EXPIRES_IN,
+  });
 
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
   });
 });
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) check if email and password exist
+  if (!email || !password) {
+    next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) check if user exists && password is correct
+  // 3) if everything ok, send token to client
+  const token = '';
+  res.status(200);
+};
--- natours/models/userModel.js
+++ natours/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -25,6 +26,12 @@ const userSchema = new mongoose.Schema(
     passwordConfirm: {
       type: String,
       required: [true, 'Please confirm your password'],
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same',
+      },
     },
   },
   {
@@ -32,6 +39,17 @@ const userSchema = new mongoose.Schema(
     toObject: { virtuals: true },
   },
 );
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  // Delete password confirm
+  this.passwordConfirm = undefined;
+  next();
+});
 
 const User = mongoose.model('User', userSchema);
 
